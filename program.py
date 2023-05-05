331ebca6786bb6cdb7b4842dfc47632ecfcce4f4
hugotaranto <92898615+hugotaranto@users.noreply.github.com>
2023-05-05 17:06:45 +1000

    Update program.py

--- program.py
+++ program.py
@@ -5,15 +5,14 @@ from matplotlib.animation import FuncAnimation
 from functools import partial
 import matplotlib.patches as patches
 
-
 # vehicle measurements in cm
 
 AXL_DISTANCE = 3.5 # distance from wheel to center
-SENSOR_LENGTH = 5 # perpendicular distance from sensor to center line
+SENSOR_LENGTH = 4 # perpendicular distance from sensor to center line
 SENSOR_WIDTH = 2.5 # distance between both sensors
 
 ROBOT_WIDTH = 7
-ROBOT_HEIGHT = 11
+ROBOT_HEIGHT = 10
 
 
 # wheel velocity in cm/s
@@ -29,8 +28,11 @@ Y_CENTER = 7.5
 ELLIPSE_A = 12.5
 ELLIPSE_B = 7.5
 
+# timestep used throughout:
 
-def vehicle_model(uk, xk, timestep):
+TIME_STEP = 0.01 # 100 steps per second
+
+def VehicleModel(uk, xk):
 
     vk = uk[0]
     turn_rate = uk[1]
@@ -39,225 +41,71 @@ def vehicle_model(uk, xk, timestep):
     y = xk[1]
     heading = xk[2]
 
-    xk[0] = x + timestep * vk * math.cos(heading)
-    xk[1] = y + timestep * vk * math.sin(heading)
+    xk[0] = x + TIME_STEP * vk * math.cos(heading)
+    xk[1] = y + TIME_STEP * vk * math.sin(heading)
 
-    xk[2] = heading + timestep * turn_rate
+    xk[2] = heading + TIME_STEP * turn_rate
 
     return xk
 
 
 def SensorModel(xk):
 
-    # print(xk[0], xk[1])
-
-    sl = [0, 0]
+    # arrays storing x and y coordinates respectively:
+    sl = [0, 0] 
     sr = [0, 0]
 
-    # length and width of Thing :
-
-
+    # setting up r and theta:
     r = math.sqrt(SENSOR_LENGTH**2 + (SENSOR_WIDTH/2)**2)
     theta = math.atan((SENSOR_WIDTH/2)/SENSOR_LENGTH)
 
-    # sl[0] = xk[0] + r * math.cos((xk[2] * math.pi/180 + theta))
-    # sl[1] = xk[1] + r * math.sin((xk[2] * math.pi/180 + theta))
-
-    # sr[0] = xk[0] + r * math.cos((xk[2] * math.pi/180 - theta))
-    # sr[1] = xk[1] + r * math.sin((xk[2] * math.pi/180 - theta))
-
-
+    # calculating coordinates for both sensors:
     sl[0] = xk[0] + r * math.cos((xk[2] + theta))
     sl[1] = xk[1] + r * math.sin((xk[2] + theta))
 
     sr[0] = xk[0] + r * math.cos((xk[2] - theta))
     sr[1] = xk[1] + r * math.sin((xk[2] - theta))
 
-
-
-
     return sl, sr
 
 
+def TrackModel(sl, sr, a, b, x0, y0, thickness):
 
-def TrackModel(sl, sr, a, b, x0, y0):
+    dl = where_point_thick(sl[0], sl[1], a, b, x0, y0, thickness)
+    dr = where_point_thick(sr[0], sr[1], a, b, x0, y0, thickness)
 
+    return dl, dr
 
-    # distance from center of ellipse to point
-    # dl = math.sqrt((sl[0] - x0)**2 + (sl[1] - y0)**2)
 
-    # find the equation of line from center to given point:
-
-    # gradient:
-
-    try:
-
-        ml = (y0 - sl[1]) / (x0 - sl[0])
-
-
-        # now find b from y = mx + b
-
-        constl = y0 - ml * x0
-
-        # we now have the equation of the line from the center of the ellipse to the given point
-
-
-        # now find the intersection of the ellipse and this line:
-
-        # solve the equations simultaneously to do this
-
-        # y = mx + const
-        # -mx + y = const
-
-        # (x - x0)**2/a**2 + (y - y0)**2/b**2 - 1
-
-        # get it into quadratic form after subbing line equation into ellipse equation:
-
-        # ax^2 + bx + c = 0
-
-
-        # where a = (b**2 + a**2 * m**2), b = (-2 * b**2 * x0 + a**2 * 2 * (c - y0) * m * x), c = b**2 * x0**2 + a**2 * (c - y0)**2 - a**2 * b**2
-
-
-        # x**2 * (b**2 + a**2 * m**2) + x * (-2 * b**2 * x0 + a**2 * 2 * (c - y0) * m * x) + b**2 * x0**2 + a**2 * (c - y0)**2 - a**2 * b**2 = 0
-
-
-        al = (b**2 + a**2 * ml**2)
-        bl = (-2 * b**2 * x0 + a**2 * 2 * (constl - y0) * ml)
-        cl = b**2 * x0**2 + a**2 * (constl - y0)**2 - a**2 * b**2
-
-        # now we can solve quadratically to find the intersect/s:
-
-
-        xlp = (-bl + math.sqrt(bl**2 - 4 * al * cl)) / (2 * al)
-        xln = (-bl - math.sqrt(bl**2 - 4 * al * cl)) / (2 * al)
-        
-        ylp = ml * xlp + constl
-        yln = ml * xln + constl
-
-
-
-
-    except(ZeroDivisionError):
-
-        # the line between the 2 points is vertical
-
-        ylp = y0 + b
-        yln = y0 - b
-
-        xlp = x0
-        xln = x0
-
-
-
-    try:
-
-        mr = (y0 - sr[1]) / (x0 - sr[0])
-        constr = y0 - mr * x0
-
-        ar = (b**2 + a**2 * mr**2)
-        br = (-2 * b**2 * x0 + a**2 * 2 * (constr - y0) * mr)
-        cr = b**2 * x0**2 + a**2 * (constr - y0)**2 - a**2 * b**2
-
-        xrp = (-br + math.sqrt(br**2 - 4 * ar * cr)) / (2 * ar)
-        xrn = (-br - math.sqrt(br**2 - 4 * ar * cr)) / (2 * ar)
-        
-        yrp = mr * xrp + constr
-        yrn = mr * xrn + constr
-
-    except(ZeroDivisionError):
-
-        yrp = y0 + b
-        yrn = y0 - b
-
-        xrp = x0
-        xrn = x0
-
-
-
-    # now we find the distance from the given point to each of these points:
-
-    dlp = math.sqrt((sl[0] - xlp)**2 + (sl[1] - ylp)**2)
-    dln = math.sqrt((sl[0] - xln)**2 + (sl[1] - yln)**2)
-
-    drp = math.sqrt((sr[0] - xrp)**2 + (sr[1] - yrp)**2)
-    drn = math.sqrt((sr[0] - xrn)**2 + (sr[1] - yrn)**2)
-
-
-    where_l = where_point(sl[0], sl[1], a, b, x0, y0)
-    where_r = where_point(sr[0], sr[1], a, b, x0, y0)
-
-    # print(where_l, where_r)
-
-
-    rl = min(dlp, dln)
-    rr = min(drp, drn)
-
-    # if rl < ELLIPSE_THICKNESS / 2:
-    #     rl = 0
-    # else:
-    #     rl = rl - ELLIPSE_THICKNESS / 2
-
-    # if rr < ELLIPSE_THICKNESS / 2:
-    #     rr = 0
-    # else:
-    #     rr = rr - ELLIPSE_THICKNESS / 2
-
-
-
-    if where_l < 0:
-        rl = -rl
-
-    if where_r < 0:
-        rr = -rr
-
-
-
-    return rl, rr
-
-
-
-def ControlModel(sl, sr):
-
+def ControlModel(dl, dr):
 
     vl = MAX_WHEEL_VELOCITY
     vr = MAX_WHEEL_VELOCITY
 
-    if sr == 0:
+    if dr == 0:
         vr = 0 
     
-    if sl == 0:
+    if dl == 0:
         vl = 0
     
     return vl, vr
 
 
-
 def ControlModelProportional(dl, dr):
-
-
-    # we have to make up a formula to convert distance into wheel speeds
-
 
     # outside wheel should always be faster
 
     vl = MAX_WHEEL_VELOCITY
     vr = MAX_WHEEL_VELOCITY
 
+    if dl < 0: # left wheel is inside the ellipse 
+        vl = MAX_WHEEL_VELOCITY * 2/math.pi * math.atan(abs(dl) * 100) # gives a value from 0 to max wheel velocity
 
-
-    if dl < 0 and abs(dl) < 2: # left wheel is inside the ellipse 
-        vl = MAX_WHEEL_VELOCITY * 2/math.pi * math.atan(abs(dl) * 3)
-
-    if dr < 0 and abs(dl) < 2:
-        vr = MAX_WHEEL_VELOCITY * 2/math.pi * math.atan(abs(dr) * 3)
+    if dr < 0: # right wheel is inside the ellipse
+        vr = MAX_WHEEL_VELOCITY * 2/math.pi * math.atan(abs(dr) * 100)
 
 
     return vl, vr
-
-
-
-
 
 
 def where_point(x, y, a, b, x0, y0):
@@ -267,7 +115,7 @@ def where_point(x, y, a, b, x0, y0):
 
 def where_point_thick(x, y, a, b, x0, y0, thickness):
 
-    # now we have 2 elipses where a and b of each are offset by the given thickness
+    # now we have 2 elipses where a and b of each are offset by the given thickness / 2
 
     a_inner = a - thickness / 2
     b_inner = b - thickness / 2
@@ -275,45 +123,65 @@ def where_point_thick(x, y, a, b, x0, y0, thickness):
     a_outer = a + thickness / 2
     b_outer = b + thickness / 2
 
+    # now find the results of the function given the point and these 2 ellipses
+
     result_inner = (x - x0)**2/a_inner**2 + (y - y0)**2/b_inner**2 - 1
     result_outer = (x - x0)**2/a_outer**2 + (y - y0)**2/b_outer**2 - 1
 
+    # if the result on the outer ellipse is > 0 
+    # we know the point is outside the thick ellipse
     if result_outer > 0:
         return result_outer
     
+    # if the result on the inner ellipse is < 0 
+    # we know the point is inside the thick ellipse
     if result_inner < 0:
         return result_inner
     
+    # otherwise the point is on the ellipse
+
     return 0
 
 
+def simulate(vr, vl, x, y, heading, duration, ax):
 
-def simulate(d, vr, vl, x, y, heading, time, time_step):
+    # calculate velocity and turn rate given wheel speeds:
 
-    t = 0
-
-    turn_rate = (vr - vl) / d
     vk = (vr + vl) / 2
+    turn_rate = (vr - vl) / AXL_DISTANCE
+    
+    # put results into arrays
 
     uk = [vk, turn_rate]
     xk = [x, y, heading]
 
-    while t < time:
+    x_poses = []
+    y_poses = []
 
-        t += time_step
+    t = 0
 
-        xk = vehicle_model(uk, xk, time_step)
+    # loop through until t < given time, t is incremented by TIME_STEP every iteration
 
+    while t < duration:
+
+        t += TIME_STEP
+
+        # put vehicle x and y coordinates into lists
         x_poses.append(xk[0])
         y_poses.append(xk[1])
 
-        # print_thing(x, y, pitchfork_k, t)
 
+        # update xk to be xk + 1
+        xk = VehicleModel(uk, xk)
+
+        
+    # after loop has finished plot the stored x and y coordinates
     ax.plot(x_poses, y_poses)
 
 
+def ellipse_simulate(x0, y0, a, b, gap, ax):
 
-def elipse_simulate(x0, y0, a, b, thickness, step):
+    # setting up the range of the x and y values, we take a and b and extend them by 5:
 
     x_range = [x0 - a - 5, x0 + a + 5]
     y_range = [y0 - b - 5, y0 + b + 5]
@@ -321,78 +189,121 @@ def elipse_simulate(x0, y0, a, b, thickness, step):
     x = x_range[0]
     y = y_range[0]
 
+    # setting up arrays to store values
+
+    x_poses = []
+    y_poses = []
+    result = []
+
+    # now at each x value loop through all of the y values incrementing by the given gap
+
     while x < x_range[1]:
 
         while y < y_range[1]:
 
+            # add the coordinates to the arrays
+
             x_poses.append(x)
             y_poses.append(y)
 
-            result.append(where_point_thick(x, y, a, b, x0, y0, thickness))
+            # find the result of where the point is:
 
-            y += step
+            result.append(where_point(x, y, a, b, x0, y0))
 
+            y += gap
 
-        x += step
+        # go to next x value and reset y to initial
+        x += gap
         y = y_range[0]
 
-    
 
-# arrays to store the x and y positions of the vehicle
+    # now plot the values, if the result was on the ellipse plot a black point
 
-x_poses = []
-y_poses = []
+    # if the point is outside the ellipse plot a reddish point
 
+    # if the point is inside the ellipse plot a blueish point
 
-# array to store score of where point is // used in ellipse simulate
-result = []
+    for i in range(len(x_poses)):
 
-# elipse_simulate(0, 7.5, 12.5, 7.5, 1.5, 1)
+        if result[i] == 0: # on ellipse
+            colour = 'black'
+        elif result[i] > 0: # outside
 
+            # we use this arctan function to give a result between 0 and 1
 
-# for i in range(len(x_poses)):
+            colour = [(2/math.pi * math.atan(result[i] * 2), 0.5, 0)] 
+        else: # inside
+            colour = [(0, 0.5, 2/math.pi * math.atan(abs(result[i]) * 2))]
 
-#     if result[i] == 0:
-#         colour = 'green'
-#     elif result[i] > 0:
-#         colour = 'red'
-#     else:
-#         colour = 'blue'
+        ax.scatter(x_poses[i], y_poses[i], color=colour, s=18)
 
 
-#     plt.scatter(x_poses[i], y_poses[i], color = colour, s = 18)
+def ellipse_simulate_thick(x0, y0, a, b, gap, thickness, ax):
+
+    # setting up the range of the x and y values, we take a and b and extend them by 5:
+
+    x_range = [x0 - a - 5, x0 + a + 5]
+    y_range = [y0 - b - 5, y0 + b + 5]
+
+    x = x_range[0]
+    y = y_range[0]
+
+    # setting up arrays to store values
+
+    x_poses = []
+    y_poses = []
+    result = []
+
+    # now at each x value loop through all of the y values incrementing by the given gap
+
+    while x < x_range[1]:
+
+        while y < y_range[1]:
+
+            # add the coordinates to the arrays
+
+            x_poses.append(x)
+            y_poses.append(y)
+
+            # find the result of where the point is:
+
+            result.append(where_point_thick(x, y, a, b, x0, y0, thickness))
+
+            y += gap
+
+        # go to next x value and reset y to initial
+        x += gap
+        y = y_range[0]
 
 
-# ELLIPSE_THICKNESS = 0.015
-# X_CENTER = 0
-# Y_CENTER = 0.075
-# ELLIPSE_A = 0.125
-# ELLIPSE_B = 0.075
+    # now plot the values, if the result was on the ellipse plot a black point
+
+    # if the point is outside the ellipse plot a reddish point
+
+    # if the point is inside the ellipse plot a blueish point
+
+    for i in range(len(x_poses)):
+
+        if result[i] == 0: # on ellipse
+            colour = 'black'
+        elif result[i] > 0: # outside
+
+            # we use this arctan function to give a result between 0 and 1
+
+            colour = [(2/math.pi * math.atan(result[i] * 2), 0.5, 0)] 
+        else: # inside
+            colour = [(0, 0.5, 2/math.pi * math.atan(abs(result[i]) * 2))]
+
+        ax.scatter(x_poses[i], y_poses[i], color=colour, s=18)
 
 
-xk = [X_CENTER, Y_CENTER + ELLIPSE_B, 0]
-
-
-
-# simulate(AXL_DISTANCE, MAX_WHEEL_VELOCITY, 0, 0, 0, 0, 10, 0.1) # simulating part d
-
-
-# simulate_full_bangbang([0, 15, 0], ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER, ELLIPSE_THICKNESS, 10, 0.1)
-
-
-# plotting the ellipse
-
-
-def make_plot(xk, x_poses, y_poses, sl, sr):
-
+def make_plot(xk, x_poses, y_poses, sl, sr, ax):
 
     ax.cla()
     ax.grid(color='lightgray')
     ax.set_axisbelow(True)
 
-
     # creating the inner and outer ellipses:
-
     t = np.linspace(0, 2*math.pi, 100)
 
     x_inner = X_CENTER + (ELLIPSE_A - ELLIPSE_THICKNESS / 2)*np.cos(t)
@@ -401,146 +312,131 @@ def make_plot(xk, x_poses, y_poses, sl, sr):
     x_outer = X_CENTER + (ELLIPSE_A + ELLIPSE_THICKNESS / 2)*np.cos(t)
     y_outer = Y_CENTER + (ELLIPSE_B + ELLIPSE_THICKNESS / 2)*np.sin(t)
 
-
     # creating the track by filling space between both ellipses
-
     ax.fill_between(x_inner, y_outer, y_inner, facecolor="black")
-
     ax.fill_betweenx(y_outer, x_outer, x_inner, facecolor="black")
-
-
 
     # plotting path of vehicle
     ax.plot(x_poses, y_poses, color="blue", zorder=1)
 
     # creating outline of vehicle
-    rect = patches.Rectangle((xk[0] - ROBOT_HEIGHT/2, xk[1] - ROBOT_WIDTH/2), ROBOT_HEIGHT, ROBOT_WIDTH, linewidth=1, edgecolor="blue", facecolor="none", angle=xk[2] * 180/math.pi, rotation_point='center')
+    rect = patches.Rectangle((xk[0] - ROBOT_HEIGHT/2, xk[1] - ROBOT_WIDTH/2), 
+                             ROBOT_HEIGHT, ROBOT_WIDTH, linewidth=1, edgecolor="blue", 
+                             facecolor="none", angle=xk[2] * 180/math.pi, 
+                             rotation_point='center')
 
     ax.add_patch(rect)
 
     # plotting points of vehicle position, and sensor positions
-
     ax.scatter(xk[0], xk[1], color="blue", s=12)
     ax.scatter(sl[0], sl[1], color="green", s=15, zorder=2)
     ax.scatter(sr[0], sr[1], color="red", s=15, zorder=2)
 
-
     # putting a point in each corner +5 to stop annoying resizing
-
     #bottom left
     ax.scatter(X_CENTER - ELLIPSE_A - 5, Y_CENTER - ELLIPSE_B - 5, color="none")
-
     # top right
-
     ax.scatter(X_CENTER + ELLIPSE_A + 5, Y_CENTER + ELLIPSE_B + 5, color="none")
 
 
 
+def animate_bangbang(frame, x_poses, y_poses, lap_frames, xk, ax):
 
+    # this is keeping track of the number of frames that have ellapsed when
+    # the robot passes through and x coordinate of 0
+    if frame != 0:
+        if xk[0] > 0 and x_poses[-1] < 0:
+            lap_frames.append(frame)
 
+    # append the current position onto the positions array
+    x_poses.append(xk[0])
+    y_poses.append(xk[1])
 
-def animate_bangbang(frame, xk, timestep):
-
-
-    print("Heading:", xk[2])
-
+    # set the sensor positions
     sl, sr = SensorModel(xk)
 
-    # dl, dr = TrackModel(sl, sr, a, b, x0, y0)
+    # now that we have sensor positions we can make the plot of the current frame.
+    make_plot(xk, x_poses, y_poses, sl, sr, ax)
 
-    # vl, vr = ControlModelProportional(dl, dr)
-    make_plot(xk, x_poses, y_poses, sl, sr)
+    # get the distance from each sensor to the ellipse:
+    dl, dr = TrackModel(sl, sr, ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER, 
+                        ELLIPSE_THICKNESS)
 
+    # calculate the velocities of the robot:
+    vl, vr = ControlModel(dl, dr)
 
-    vl, vr = ControlModel(where_point_thick(sl[0], sl[1], ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER, ELLIPSE_THICKNESS), 
-                          where_point_thick(sr[0], sr[1], ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER, ELLIPSE_THICKNESS))
-
-
+    # create uk given the velocities:
     turn_rate = (vr - vl) / AXL_DISTANCE
     vk = (vr + vl) / 2
 
     uk = [vk, turn_rate]
 
+    # and calculate xk + 1
+    xk = VehicleModel(uk, xk)
 
-    xk = vehicle_model(uk, xk, timestep)
+    # this will then be looped again with the new xk value for the next frame.
 
+
+def animate_proportional(frame, x_poses, y_poses, lap_frames, xk, ax):
+
+    # this is keeping track of the number of frames that have ellapsed when
+    # the robot passes through and x coordinate of 0
+    if frame != 0:
+        if xk[0] > 0 and x_poses[-1] < 0:
+            lap_frames.append(frame)
+
+    # append the current position onto the positions array
     x_poses.append(xk[0])
     y_poses.append(xk[1])
 
-
-
-
-
-
-def animate_proportional(frame, xk, timestep):
-
+    # set the sensor positions
     sl, sr = SensorModel(xk)
 
-    make_plot(xk, x_poses, y_poses, sl, sr)
+    # now that we have sensor positions we can make the plot of the current frame.
+    make_plot(xk, x_poses, y_poses, sl, sr, ax)
 
-    dl, dr = TrackModel(sl, sr, ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER)
+    # get the distance from each sensor to the ellipse:
+    dl, dr = TrackModel(sl, sr, ELLIPSE_A, ELLIPSE_B, X_CENTER, Y_CENTER, 
+                        ELLIPSE_THICKNESS)
 
+    # calculate the velocities of the robot:
     vl, vr = ControlModelProportional(dl, dr)
 
+    # create uk given the velocities:
     turn_rate = (vr - vl) / AXL_DISTANCE
     vk = (vr + vl) / 2
 
     uk = [vk, turn_rate]
 
+    # and calculate xk + 1
+    xk = VehicleModel(uk, xk)
 
-    xk = vehicle_model(uk, xk, timestep)
+    # this will then be looped again with the new xk value for the next frame.
 
-    x_poses.append(xk[0])
-    y_poses.append(xk[1])
-
-
-
+    
+# initial xk value
+xk = [0, 0, 0]
 fig, ax = plt.subplots()
 
+x_poses = []
+y_poses = []
 
-ani = FuncAnimation(plt.gcf(), partial(animate_proportional, xk=xk, timestep=0.01), interval=1, cache_frame_data=False)
+# array to store the frame numbers when the robot passes from a negative x value to a positive
+lap_frames = []
 
+# function from matplotlib.animation library to animate matplots
 
-# ani = FuncAnimation(plt.gcf(), partial(animate_bangbang, xk=xk, timestep=0.01), interval=10, cache_frame_data=False)
+ani = FuncAnimation(plt.gcf(), partial(animate_proportional, 
+                                       x_poses=x_poses, 
+                                       y_poses=y_poses, 
+                                       lap_frames=lap_frames, xk=xk, ax=ax), 
+                                       interval=10, cache_frame_data=False)
 
-
-
-# simulate(AXL_DISTANCE, MAX_WHEEL_VELOCITY, 0, 0, 0, 0, 10, 0.01)
-
-
-
-ax.set_aspect('equal')
+ax.set_aspect("equal")
 plt.show()
 
 
-
-
-# ani = FuncAnimation(plt.gcf(), simulate_full_proportional())
-
-
-
-# #plotting:
-
-
-
-# x = 12
-# y = 1000
-
-# print(TrackModel([x, y], [x, y], 12.5, 7.5, 0, 7.5))
-# print(where_point(x, y, 12.5, 7.5, 0, 7.5))
-
-
-# plt.scatter(x, y, color = 'black', s = 25)
-
-
-
-# for full_simulate_proportional
-
-# for i in range(len(x_poses)):
-#     plt.scatter(x_poses[i], y_poses[i])
-
-
-
-
-
+# this is printing out the times that each lap took.
+for i in range(len(lap_frames)):
+    print("time for loop {}: {}s".format(i + 1, lap_frames[i] * TIME_STEP))
 
